332fce00e7a0f12c2e3bcdffd4febe918bf0c255
Hites Dániel <hitesdani@gmail.com>
2020-12-10 09:31:22 +0100

    project steps added

---
+++ script.js
@@ -0,0 +1,34 @@
+function pageLoad() {
+    /*
+    To do appot készítünk, alapértelmezésben 10 üres mező áll rendelkezésre, de hozzá tudunk adni újakat és minden mezőben van egy checkbox
+    Text input mező, amibe írhatjuk a feladatot és egy törlés gomb, amivel ki tudjuk törölni a sort.
+    */
+
+    //Hozzáadom a rootot
+
+
+    //El kell tárolnunk a maximális számot egy változóban (10)
+
+
+    //div befoglaló elem, checkbox, text input, delete gomb (HTML struktúra eltárolása egy változóba)
+
+
+    //for ciklussal 10 sort hozunk létre, hozzáadjuk a felülethez(insertAdjestHTML)
+
+
+    //gomb hozzáadása alulra(insertAdjestHTML beállítása afterend, hogy a rooton kívülre kerüljön)
+
+
+    //click esemény hozzáadása, változóban hozzáadadott sor hozzáillesztése a root elemhez
+
+
+    //classnév alapján kijelölöm az összes delete gombot és elmentem egy változóba
+
+
+    //for ciklussal mindegyik delete gombra adunk click eseményt
+    //hogyan fogom tudni kijelölni azt az elemet amit majd ki szeretnék törölni?
+
+
+}
+
+window.addEventListener("load", pageLoad);
